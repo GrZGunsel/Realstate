30d9ea5a036de3f5a19e6790532ef9a1ef33836d
Your Name <kshitzlama51@gmai.com>
2021-11-10 12:21:43 +0545

    sda

---
+++ Js/Main.js
@@ -0,0 +1,16 @@
+const navslide = () => {
+  const button = document.querySelector(".button");
+  const nav = document.querySelector(".navlink");
+  const background = document.querySelector(".navlinks");
+  const removebtn = document.querySelector(".navlinks");
+  console.log(nav);
+  button.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    background.classList.toggle("bg-active");
+  });
+  removebtn.addEventListener("click", () => {
+    nav.classList.remove("nav-active");
+    background.classList.remove("bg-active");
+  });
+};
+navslide();
