4d3614bf5cb353c310e5c663f25f726d98186f5e
Your Name <kshitzlama51@gmai.com>
2021-11-14 20:11:29 +0545

    lol

--- Js/Main.js
+++ Js/Main.js
@@ -3,7 +3,7 @@ const navslide = () => {
   const nav = document.querySelector(".navlink");
   const background = document.querySelector(".navlinks");
   const removebtn = document.querySelector(".navlinks");
-  console.log(nav);
+
   button.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
     background.classList.toggle("bg-active");
@@ -19,3 +19,18 @@ function slider1(id) {
   let image = document.getElementById(id).src;
   document.getElementById("demo").src = image;
 }
+const filter = () => {
+  const filterbtn = document.querySelector(".filterbutton");
+  console.log(filterbtn);
+  const Filter = document.querySelector(".advanceFilter");
+  console.log(Filter);
+  const removeclass = document.querySelector(".fas fa-filter");
+  console.log(removeclass);
+  filterbtn.addEventListener("click", () => {
+    Filter.classList.add("filter-active");
+  });
+  removeclass.addEventListener("click", () => {
+    Filter.classList.remove("nav-active");
+  });
+};
+filter();
